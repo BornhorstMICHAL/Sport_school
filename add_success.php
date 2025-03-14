91cd3cb4431efa7a7bb6f2693aa1cac551551472
BornhorstMICHAL <144802672+BornhorstMICHAL@users.noreply.github.com>
2025-03-14 21:11:32 +0100

    Update add_success.php

--- add_success.php
+++ add_success.php
@@ -7,12 +7,12 @@
 </head>
 <body>
     <h2>Přidat sportovní úspěch</h2>
-    <form action="save_success.php" method="post">
+    <form action="save_success.php" method="post" enctype="multipart/form-data">
         Jméno: <input type="text" name="student_name" required><br>
         Třída: <input type="text" name="class" required><br>
         Úspěch: <textarea name="achievement" required></textarea><br>
         Datum: <input type="date" name="event_date" required><br>
-        URL fotografie: <input type="text" name="photo_url" required><br>
+        Vyberte obrázek k nahrání:<input type="file" name="image" accept="image/*" required><br>
         <input type="submit" value="Přidat">
     </form>
 </body>
